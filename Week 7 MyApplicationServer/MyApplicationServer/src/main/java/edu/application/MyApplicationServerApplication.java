2392519f330dca14500686b4df23512489448dda
P Avinash <248902254+vtu26235@users.noreply.github.com>
2026-03-24 22:47:41 +0530

    Add files via upload

---
+++ Week 7 MyApplicationServer/MyApplicationServer/src/main/java/edu/application/MyApplicationServerApplication.java
@@ -0,0 +1,15 @@
+package edu.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class MyApplicationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyApplicationServerApplication.class, args);
+	}
+
+}
