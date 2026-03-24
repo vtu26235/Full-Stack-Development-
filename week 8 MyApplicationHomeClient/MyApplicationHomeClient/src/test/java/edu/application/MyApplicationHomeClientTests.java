534350302b64348a08f14ff4529c59981db63f5d
P Avinash <248902254+vtu26235@users.noreply.github.com>
2026-03-24 22:45:39 +0530

    Add files via upload

---
+++ week 8 MyApplicationHomeClient/MyApplicationHomeClient/src/test/java/edu/application/MyApplicationHomeClientTests.java
@@ -0,0 +1,13 @@
+package edu.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyApplicationHomeClientTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
