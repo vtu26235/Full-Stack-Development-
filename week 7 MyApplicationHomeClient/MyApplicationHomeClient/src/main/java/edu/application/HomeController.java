4c6dffd7a5c91c249e8c0710c1e0ee7bd2a00c51
P Avinash <248902254+vtu26235@users.noreply.github.com>
2026-03-24 22:37:11 +0530

    Add files via upload

---
+++ week 7 MyApplicationHomeClient/MyApplicationHomeClient/src/main/java/edu/application/HomeController.java
@@ -0,0 +1,12 @@
+package edu.application;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	@GetMapping("home")
+	public String msg() {
+		return "Hello";
+	}
+}
---
+++ week 7 MyApplicationHomeClient/MyApplicationHomeClient/src/main/java/edu/application/MyApplicationHomeClient.java
@@ -0,0 +1,13 @@
+package edu.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyApplicationHomeClient {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyApplicationHomeClient.class, args);
+	}
+
+}
