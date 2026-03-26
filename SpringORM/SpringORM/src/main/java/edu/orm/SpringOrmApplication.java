3f51592ed2a523febd09bc39b710e7b2daeb5c31
P Avinash <248902254+vtu26235@users.noreply.github.com>
2026-03-26 13:14:22 +0530

    Add files via upload

---
+++ SpringORM/SpringORM/src/main/java/edu/orm/SpringOrmApplication.java
@@ -0,0 +1,13 @@
+package edu.orm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOrmApplication.class, args);
+	}
+
+}
---
+++ SpringORM/SpringORM/src/test/java/edu/orm/SpringOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.orm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
